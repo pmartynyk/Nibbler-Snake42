a55f7078bc51aebc5cc8d6dc385548bee39842c0
Pavlo MARTYNYK <pmartyny@e1r5p10.unit.ua>
2019-08-03 14:54:54 +0300

    in progress

---
+++ includes/IDynamicLibrary.hpp
@@ -0,0 +1,11 @@
+#ifndef IDYNAMICLIBRARY_HPP
+# define IDYNAMICLIBRARY_HPP
+
+class IDynamicLibrary
+{
+public:
+    IDynamicLibrary(void);
+    ~IDynamicLibrary(void);
+};
+
+#endif
---
+++ ncurses/Ncurses.class..hpp
@@ -0,0 +1,4 @@
+#ifndef NCURSES_CLASS_HPP
+# define NCURSES_CLASS_HPP
+
+#endif
